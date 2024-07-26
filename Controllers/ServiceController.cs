ee984e906dbb84919c0da463ec237a806466bf0b
Kingsley Leung <kingsleyleung2003@outlook.com>
2024-07-27 11:13:46 +1200

    Implement GetAllPerformers

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -23,17 +23,17 @@ namespace concert_booking_service_csharp.Controllers
         [HttpPost("Register")]
         public ActionResult<string> Register(UserDTO input)
         {
-            if (_repository.GetUserByUserName(input.userName) == null)
+            if (_repository.GetUserByUserName(input.UserName) == null)
             {
                 //User user = _repository.AddUserHashed(input.userName, input.password);
-                PasswordHasherUtil.CreatePasswordHash(input.password, out byte[] passwordHash, out byte[] passwordSalt);
-                User user = new User { UserName = input.userName, PasswordHash = passwordHash, PasswordSalt = passwordSalt, Version = 1 };
+                PasswordHasherUtil.CreatePasswordHash(input.Password, out byte[] passwordHash, out byte[] passwordSalt);
+                User user = new User { UserName = input.UserName, PasswordHash = passwordHash, PasswordSalt = passwordSalt, Version = 1 };
                 User addedUser = _repository.AddUser(user);
                 return Ok("User successfully registered.");
             }
             else
             {
-                return BadRequest("UserName " + input.userName + " is not available.");
+                return BadRequest("UserName " + input.UserName + " is not available.");
             }
         }
 
@@ -44,6 +44,21 @@ namespace concert_booking_service_csharp.Controllers
         {
             return Ok("Login successful");
         }
+
+        [HttpGet("Performers")]
+        public ActionResult GetAllPerformers()
+        {
+            IEnumerable<Performer> performers = _repository.GetAllPerformers();
+            List<PerformerDTO> performerDTOs = new List<PerformerDTO>();
+            foreach (Performer performer in performers)
+            {
+                performerDTOs.Add(new PerformerDTO { PerformerId = performer.PerformerId, Name = performer.Name, ImageName = performer.ImageName, Genre = performer.Genre, Blurb = performer.Blurb });
+            }
+            return Ok(performerDTOs);
+        }
+
+
+        //[HttpGet("Performers/{id}")]
 
 
     }
