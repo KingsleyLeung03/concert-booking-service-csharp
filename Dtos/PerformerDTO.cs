2514665982f88b4b0ed04ad7ceb9e856ab20bcc2
Kingsley Leung <kingsleyleung2003@outlook.com>
2024-07-27 10:20:05 +1200

    Update PerformerDTO to implement IComparable

--- Dtos/PerformerDTO.cs
+++ Dtos/PerformerDTO.cs
@@ -1,13 +1,38 @@
 ﻿using concert_booking_service_csharp.Enums;
+using System.Xml.Linq;
 
 namespace concert_booking_service_csharp.Dtos
 {
-    public class PerformerDTO
+    public class PerformerDTO : IComparable<PerformerDTO>
     {
         public long performerId { get; set; }
         public string name { get; set; }
         public string imageName { get; set; }
         public Genre genre { get; set; }
         public string blurb { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj) return true;
+
+            if (obj == null || GetType() != obj.GetType()) return false;
+
+            PerformerDTO other = (PerformerDTO)obj;
+
+            return performerId == other.performerId &&
+                   name == other.name &&
+                   imageName == other.imageName &&
+                   genre == other.genre;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(performerId, name, imageName, genre);
+        }
+
+        public int CompareTo(PerformerDTO other)
+        {
+            return other.name.CompareTo(name);
+        }
     }
 }
