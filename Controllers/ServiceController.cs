54e2cd776220e27f38914f3e552c84cd24cc06dc
Kingsley Leung <kingsleyleung2003@outlook.com>
2024-07-28 16:37:03 +1200

    Implement GetPerformerById, GetAllConcerts, GetConcertById, GetConcertSummaries

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -52,13 +52,131 @@ namespace concert_booking_service_csharp.Controllers
             List<PerformerDTO> performerDTOs = new List<PerformerDTO>();
             foreach (Performer performer in performers)
             {
-                performerDTOs.Add(new PerformerDTO { PerformerId = performer.PerformerId, Name = performer.Name, ImageName = performer.ImageName, Genre = performer.Genre, Blurb = performer.Blurb });
+                performerDTOs.Add(new PerformerDTO 
+                { 
+                    PerformerId = performer.PerformerId, 
+                    Name = performer.Name, 
+                    ImageName = performer.ImageName, 
+                    Genre = performer.Genre, 
+                    Blurb = performer.Blurb 
+                });
             }
             return Ok(performerDTOs);
         }
 
+        [HttpGet("Performers/{id}")]
+        public ActionResult GetPerformerById(int id)
+        {
+            Performer performer = _repository.GetPerformerById(id);
+            if (performer != null)
+            {
+                PerformerDTO performerDTO = new PerformerDTO 
+                { 
+                    PerformerId = performer.PerformerId, 
+                    Name = performer.Name, 
+                    ImageName = performer.ImageName, 
+                    Genre = performer.Genre, 
+                    Blurb = performer.Blurb 
+                };
+                return Ok(performerDTO);
+            }
+            return NotFound("Performer " + id + " does not exist.");
+        }
 
-        //[HttpGet("Performers/{id}")]
+        [HttpGet("Concerts")]
+        public ActionResult GetAllConcerts()
+        {
+            IEnumerable<Concert> concerts = _repository.GetAllConcerts();
+            List<ConcertDTO> concertDTOs = new List<ConcertDTO>();
+            foreach (Concert concert in concerts)
+            {
+                ICollection<ConcertDate> concertDates = concert.ConcertDates;
+                List<DateTime> dates = new List<DateTime>();
+                foreach (ConcertDate concertDate in concertDates)
+                {
+                    dates.Add(concertDate.Date);
+                }
+                ICollection<Performer> performers = concert.Performers;
+                List<PerformerDTO> performerDTOs = new List<PerformerDTO>();
+                foreach (Performer performer in performers)
+                {
+                    performerDTOs.Add(new PerformerDTO
+                    {
+                        PerformerId = performer.PerformerId,
+                        Name = performer.Name,
+                        ImageName = performer.ImageName,
+                        Genre = performer.Genre,
+                        Blurb = performer.Blurb
+                    });
+                }
+                ConcertDTO concertDTO = new ConcertDTO
+                {
+                    ConcertId = concert.ConcertId,
+                    Title = concert.Title,
+                    ImageName = concert.ImageName,
+                    Blurb = concert.Blurb,
+                    Dates = dates,
+                    Performers = performerDTOs
+                };
+            }
+            return Ok(concertDTOs);
+        }
+
+        [HttpGet("Concerts/{id}")]
+        public ActionResult GetConcertById(int id)
+        {
+            Concert concert = _repository.GetConcertById(id);
+            if (concert != null)
+            {
+                ICollection<ConcertDate> concertDates = concert.ConcertDates;
+                List<DateTime> dates = new List<DateTime>();
+                foreach (ConcertDate concertDate in concertDates)
+                {
+                    dates.Add(concertDate.Date);
+                }
+                ICollection<Performer> performers = concert.Performers;
+                List<PerformerDTO> performerDTOs = new List<PerformerDTO>();
+                foreach (Performer performer in performers)
+                {
+                    performerDTOs.Add(new PerformerDTO 
+                    { 
+                        PerformerId = performer.PerformerId, 
+                        Name = performer.Name, 
+                        ImageName = performer.ImageName, 
+                        Genre = performer.Genre, 
+                        Blurb = performer.Blurb 
+                    });
+                }
+                ConcertDTO concertDTO = new ConcertDTO 
+                { 
+                    ConcertId = concert.ConcertId, 
+                    Title = concert.Title, 
+                    ImageName = concert.ImageName, 
+                    Blurb = concert.Blurb, 
+                    Dates = dates,
+                    Performers = performerDTOs
+                };
+                return Ok(concertDTO);
+            }
+            return NotFound("Concert " + id + " does not exist.");
+        }
+
+        [HttpGet("Concerts/Summaries")]
+        public ActionResult GetConcertSummaries()
+        {
+            IEnumerable<Concert> concerts = _repository.GetAllConcerts();
+            List<ConcertSummaryDTO> concertSummaryDTOs = new List<ConcertSummaryDTO>();
+            foreach(Concert concert in concerts)
+            {
+                concertSummaryDTOs.Add(new ConcertSummaryDTO
+                {
+                    ConcertId = concert.ConcertId,
+                    Title = concert.Title,
+                    ImageName = concert.ImageName
+                });
+            }
+            return Ok(concertSummaryDTOs);
+        }
 
 
     }
